3e984098d2556a47607856e2ef3811958748d518
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-16 23:41:19 +0000

    Add 5 queries to work with our database

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,8 @@
+SELECT reservations.*, properties.*, AVG(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 9 AND end_date < Now()::date
+GROUP BY reservations.id, properties.id
+ORDER BY start_date
+LIMIT 10;

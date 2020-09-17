60f0e73774e432181997fbe27b601541dec20629
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-17 18:39:15 +0000

    Change the router.get /properties to also pass the user_id in the request, so we can render the search results properly

--- LightBnB_WebApp/server/apiRoutes.js
+++ LightBnB_WebApp/server/apiRoutes.js
@@ -1,6 +1,7 @@
 module.exports = function(router, database) {
 
   router.get('/properties', (req, res) => {
+    req.query.owner_id = req.session.userId
     database.getAllProperties(req.query, 20)
     .then(properties => res.send({properties}))
     .catch(e => {

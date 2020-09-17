ee8b9a11c29f0fd919fbbf0b8218bebda1a17ad5
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-17 15:50:53 +0000

    Change the addUser function to work with the database instead of js objects

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -78,11 +78,25 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+// const addUser = function (user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// }
+// exports.addUser = addUser;
+
+const addUser = user => {
+  const query = {
+    text: `
+      INSERT INTO users (name, email, password)
+      VALUES ($1, $2, $3) RETURNING *;  
+    `,
+    values: [user.name, user.email, user.password]
+  }
+  return pool
+  .query(query)
+  .then(res => res.rows)
 }
 exports.addUser = addUser;
 
